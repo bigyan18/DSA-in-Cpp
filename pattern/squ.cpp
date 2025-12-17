3b3bbab5388e2916a752935af865bb912fc990ce
kazi.bigyan <thapabigyan1235@gmail.com>
2025-12-17 13:26:11 +0300

    Initial DSA commit

---
+++ pattern/squ.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int inner;
+    cin>>inner;
+    for(int i=0;i<inner;i++){
+        for(int j=0;j<inner;j++){
+            cout<<j;
+
+        }
+        cout<<endl;
+    }
+    return 0;
+}
