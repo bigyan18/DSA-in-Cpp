f5cfd002263e436e5bc7385181d20240848bd487
bigyan18 <thapabigyan1235@gmail.com>
2026-03-16 22:14:01 +0300

    revWordString.cpp

---
+++ string/revWordString.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+string reverseWord(string s){
+    int n=s.length();
+    string ans="";
+    reverse(s.begin(), s.end());
+    cout<<s<<endl;
+    for(int i=0;i<n;i++){
+        string word="";
+        while(i<n && s[i]!=' '){
+            word+=s[i];
+            i++;
+        }
+        cout<<word<<endl;
+        reverse(word.begin(),word.end());
+        if(word.length()>0 ) ans+=" "+ word;
+    }
+    return ans.substr(1);
+}
+
+int main(){
+    string s="kak    dela     man     ????    ";
+    cout<<reverseWord(s);
+}
