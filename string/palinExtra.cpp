a8be97f1293164c3558dcd5363d4b9c5293d847b
bigyan18 <thapabigyan1235@gmail.com>
2026-03-16 19:25:42 +0300

    palinExtra

--- string/palinExtra.cpp
+++ string/palinExtra.cpp
@@ -6,8 +6,14 @@ int main(){
     int st=0;
     int end= s.length()-1;
     while(st<=end){
-        if(!(isalnum(s[st]))) st++;
-        if(!(isalnum(s[end]))) end--;
+        if(!(isalnum(s[st]))) {
+            st++;
+            continue;
+        }
+        if(!(isalnum(s[end]))){ 
+            end--;
+            continue;
+        }
         
         if(tolower(s[st++])!=tolower(s[end--])){
             cout<<"not palindrome"<<endl;
