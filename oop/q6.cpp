6d56785a98fc8231bd292cea3e363e08292b9ede
bigyan18 <thapabigyan1235@gmail.com>
2026-04-27 01:05:51 +0300

    q6

--- oop/q6.cpp
+++ oop/q6.cpp
@@ -1,13 +1,47 @@
 // LEVEL 6 — Class With Methods That Modify State
 // Q6. Create a class Counter with:
 // private int value
-
 // increment()
-
 // decrement()
-
 // reset()
-
 // show()
-
 // Simulate a counter being used 10 times.
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Counter{
+    int value;
+    public:
+    Counter(){
+        value=0;
+    }
+    void increment(){
+        value+=1;
+    }
+    void decrement(){
+        if(value>0) value-=1;
+    }
+    void reset(){
+        value=0;
+    }
+    void show(){
+        cout<<"The val is: "<<value<<endl;
+    }
+};
+int main(){
+    Counter c1;
+     c1.increment(); // 1
+    c1.increment(); // 2
+    c1.increment(); // 3
+    c1.decrement(); // 2
+    c1.increment(); // 3
+    c1.increment(); // 4
+    c1.decrement(); // 3
+    c1.increment(); // 4
+    c1.increment(); // 5
+    c1.reset();     // 0
+
+    c1.show();
+}
