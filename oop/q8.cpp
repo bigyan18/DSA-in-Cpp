be4f8b648c32fc790e4e4b0c41f170290303b1a7
bigyan18 <thapabigyan1235@gmail.com>
2026-04-27 01:27:45 +0300

    q8

--- oop/q8.cpp
+++ oop/q8.cpp
@@ -6,3 +6,46 @@
 
 // Store N contacts in a vector.
 // Then ask the user for a name and search for that contact.
+
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+class Contact{
+    public:
+    string name;
+    string phone;
+    Contact(string name, string phone):name(name), phone(phone){};
+    
+};
+int main(){
+    vector<Contact> c;
+    int n;
+    cout<<"enter the number of contact to add: ";
+    cin>>n;
+    cin.ignore();
+    for(int i=0;i<n;i++){
+        string name, phone;
+        cout<<"Enter the name: ";
+        getline(cin, name);
+        cout<<"Enter the phone: ";
+        getline(cin,phone);
+        Contact p(name, phone);
+        c.push_back(p);
+
+    }
+    string search;
+    bool found = false;
+    cout<<"Enter the name to search";
+    getline(cin, search);
+    for(size_t i=0; c.size(); i++){
+        if(c[i].name==search){
+            cout<<"found \n";
+            cout<<"Name: "<<c[i].name<<", phone: "<<c[i].phone<<endl;
+            found =true;
+            break;
+        }
+
+    }
+}
